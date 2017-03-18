5ec55c6fc56869252cff155f59b91b5e5c97cd5c
Aaron Murray <plugins@omv-extras.org>
2017-03-18 11:01:58 -0500

    improve foreign content handling

--- var/www/openmediavault/js/omv/module/user/service/openvpn/Certificates.js
+++ var/www/openmediavault/js/omv/module/user/service/openvpn/Certificates.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014 OpenMediaVault Plugin Developers
+ * Copyright (C) 2014-2017 OpenMediaVault Plugin Developers
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -38,10 +38,12 @@ Ext.define('OMV.module.user.service.openvpn.Certificates', {
     reloadOnActivate: true,
 
     columns: [{
+        xtype: "textcolumn",
         header: _('UUID'),
         hidden: true,
         dataIndex: 'uuid'
     }, {
+        xtype: "textcolumn",
         header: _('Common name'),
         flex: 1,
         sortable: true,
